546da962a872a567f8e89bde97da8efb00c90fe1
chiefexb <chief_exb@mail.ru>
2023-10-21 02:37:50 +0300

    123

--- simple-shop-php/src/index.php
+++ simple-shop-php/src/index.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container text-center">
+    <div class="container-fluid">
+
   <div class="row">
     <div class="col-xl-2">
       <ul class="list-group">
